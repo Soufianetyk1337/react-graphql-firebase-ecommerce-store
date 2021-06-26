f748a49fa9f1d8d4268a8cde83e2f60428835a8a
Soufianetyk1337 <sz.fanel@gmail.com>
2021-06-26 20:15:54 +0100

    Handling Sign In Form Validation With Formik & Yup

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/heading-has-content */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import "./SignInStyle.scss";
+// import "./SignInStyle.scss";
+import "./Helper.scss";
 import Button from "./../Forms/Button";
 import FormInput from "./../../components/Forms/FormInput";
 import AuthWrapper from "./../AuthWrapper";
@@ -13,17 +15,17 @@ import {
   googleSignInStart,
 } from "../../redux/User/userActions";
 import { useDispatch, useSelector } from "react-redux";
-// const validateEmail =(email) =>{
-//   const valid = new RegExp(/^[^@\s]+@[^@\s]+\.[^@\s]+$/);
-//   return valid.test(email);
-// }
+import { Formik } from "formik";
+import * as Yup from "yup";
+
 const mapState = ({ user }) => ({
   currentUser: user.currentUser,
   userError: user.userError,
 });
 function SignIn(props) {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e, values) => {
+    console.log({ values });
+    const { email, password } = values;
     dispatch(emailSignInStart({ email, password }));
   };
   const { currentUser, userError } = useSelector(mapState);
@@ -41,73 +43,182 @@ function SignIn(props) {
       history.push("/");
     }
     return () => {};
-  }, [currentUser, history, dispatch, formError]);
+  }, [currentUser, history, dispatch]);
   const handeGoogleSignIn = () => {
     dispatch(googleSignInStart());
   };
   return (
+    // <AuthWrapper headline="Sign In">
+    //   <div className="formWrapper">
+    //     <form className="form" onSubmit={handleSubmit}>
+    //       {Array.isArray(formError) && formError.length > 0 && (
+    //         <div className="form-error-wrap">
+    //           <div className="form-error">{formError}</div>
+    //           <div
+    //             className="form-close"
+    //             onClick={() => {
+    //               console.log("Form error");
+    //               setFormError([]);
+    //             }}
+    //           >
+    //             X
+    //           </div>
+    //         </div>
+    //       )}
+
+    //       <div className="form-control">
+    //         <label htmlFor="email">Email Address</label>
+    //         <FormInput
+    //           type="type"
+    //           name="email"
+    //           value={email}
+    //           placeholder="Enter Your Email Address"
+    //           handleChange={(e) => setEmail(e.target.value)}
+    //         />
+    //         <i className="bx bx-comment-check"></i>
+    //         <i className="bx bx-comment-error"></i>
+    //         <small>Error Message</small>
+    //       </div>
+    //       <div className="form-control">
+    //         <label htmlFor="email">Password </label>
+    //         <FormInput
+    //           type="password"
+    //           name="password"
+    //           value={password}
+    //           placeholder="Enter Your Password"
+    //           handleChange={(e) => setPassword(e.target.value)}
+    //         />
+    //         <i className="bx bx-comment-check"></i>
+    //         <i className="bx bx-comment-error"></i>
+    //       </div>
+    //       <Button type="submit" className="form-button">
+    //         Login
+    //       </Button>
+
+    //       <Button className="form-button google" onClick={handeGoogleSignIn}>
+    //         Google SignIn
+    //       </Button>
+
+    //       <div className="links form-button">
+    //         <Link to="recovery" className="recovery">
+    //           Reset password
+    //         </Link>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </AuthWrapper>
     <AuthWrapper headline="Sign In">
       <div className="formWrapper">
-        <form className="form" onSubmit={handleSubmit}>
-          {Array.isArray(formError) && formError.length > 0 && (
-            <div className="form-error-wrap">
-              <div className="form-error">{formError}</div>
-              <div
-                className="form-close"
-                onClick={() => {
-                  console.log("Form error");
-                  setFormError([]);
-                }}
-              >
-                X
-              </div>
-            </div>
-          )}
+        <Formik
+          initialValues={{ email, password }}
+          onSubmit={async (values, event) => {
+            handleSubmit(event, values);
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string().email().required("Required"),
+            password: Yup.string()
+              .required("No password provided.")
+              .min(8, "Password is too short - should be 8 chars minimum.")
+              .matches(
+                /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+              ),
+          })}
+        >
+          {(props) => {
+            const {
+              values,
+              touched,
+              errors,
+              dirty,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              handleReset,
+            } = props;
+            return (
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="email" style={{ display: "block" }}>
+                  Email
+                </label>
+                <input
+                  id="email"
+                  placeholder="Enter your email"
+                  type="text"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.email && touched.email
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.email && touched.email && (
+                  <div className="input-feedback">{errors.email}</div>
+                )}
+                <label htmlFor="password" style={{ display: "block" }}>
+                  Password
+                </label>
+                <input
+                  id="password"
+                  placeholder="Enter your password"
+                  type="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.password && touched.password && (
+                  <div className="input-feedback">{errors.password}</div>
+                )}
 
-          <div className="form-control">
-            <label htmlFor="email">Email Address</label>
-            <FormInput
-              type="type"
-              name="email"
-              value={email}
-              placeholder="Enter Your Email Address"
-              handleChange={(e) => setEmail(e.target.value)}
-            />
-            <i className="bx bx-comment-check"></i>
-            <i className="bx bx-comment-error"></i>
-            <small>Error Message</small>
-          </div>
-          <div className="form-control">
-            <label htmlFor="email">Password </label>
-            <FormInput
-              type="password"
-              name="password"
-              value={password}
-              placeholder="Enter Your Password"
-              handleChange={(e) => setPassword(e.target.value)}
-            />
-            <i className="bx bx-comment-check"></i>
-            <i className="bx bx-comment-error"></i>
-          </div>
-          <Button type="submit" className="form-button">
-            Login
-          </Button>
+                {Array.isArray(userError) &&
+                  userError.length > 0 &&
+                  !isSubmitting && (
+                    <div className="input-feedback">{userError}</div>
+                  )}
+                <button
+                  type="button"
+                  className="outline"
+                  onClick={handleReset}
+                  disabled={!dirty || isSubmitting}
+                >
+                  Reset
+                </button>
+                <button type="submit" disabled={isSubmitting}>
+                  Login
+                </button>
 
-          <Button className="form-button google" onClick={handeGoogleSignIn}>
-            Google SignIn
-          </Button>
-
-          <div className="links form-button">
-            <Link to="recovery" className="recovery">
-              Reset password
-            </Link>
-          </div>
-        </form>
+                <DisplayFormikState {...props} />
+              </form>
+            );
+          }}
+        </Formik>
       </div>
     </AuthWrapper>
   );
 }
 
+const DisplayFormikState = (props) => (
+  <div style={{ margin: "1rem 0" }}>
+    <h3 style={{ fontFamily: "monospace" }} />
+    <pre
+      style={{
+        background: "#f6f8fa",
+        fontSize: ".65rem",
+        padding: ".5rem",
+      }}
+    >
+      <strong>props</strong> = {JSON.stringify(props, null, 2)}
+    </pre>
+  </div>
+);
 export default SignIn;
 /*
 <div className="container">
